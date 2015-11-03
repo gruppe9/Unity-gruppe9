f1444d73d65a01342d7b1851651e28fc43e1044b
Martin Klevin <mogar85@hotmail.com>
2015-11-03 12:42:32 +0100

    updated gitignore to work with unity added a 'frame' or 'help' to unit selection started making Player both gameobject and script

---
+++ Toy_box_wars_the_sand_box_conflict/Assets/Scripts/Player.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum Teams
+{
+    team1,
+    team2
+}
+public class Player : MonoBehaviour {
+
+    private GameObject selectedUnit;
+    private GameObject selectedOther;
+    private Teams currentTeam;
+
+    /// <summary>
+    /// Get/Set property for selected unit
+    /// </summary>
+    public GameObject SelectedUnit
+    {
+        get
+        {
+            return selectedUnit;
+        }
+
+        set
+        {
+            selectedUnit = value;
+        }
+    }
+
+    /// <summary>
+    /// Get/Set property for another selected unit
+    /// </summary>
+    public GameObject SelectedOther
+    {
+        get
+        {
+            return selectedOther;
+        }
+
+        set
+        {
+            selectedOther = value;
+        }
+    }
+
+    /// <summary>
+    /// Get/Set property for which team has the current team
+    /// </summary>
+    internal Teams CurrentTeam
+    {
+        get
+        {
+            return currentTeam;
+        }
+
+        set
+        {
+            currentTeam = value;
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Toy_box_wars_the_sand_box_conflict/Assets/Scripts/UnitSelector.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitSelector : MonoBehaviour
+{
+    Player playerComponent;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerComponent = GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // raycasting stuff
+
+            // brug playerComponent.SelectedUnit til at gemme referencen til den valgte unit som også har samme team som
+            // playerComponent.CurrentTeam.
+
+            // brug playerComponent.SelectedOther til at gemme referencen til den valgte unit som ikke har samme team som
+            // playerComponent.CurrentTeam.
+
+
+        }
+    }
+}
