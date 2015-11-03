c1430f6f5b0301288673536bbf750e3a9af0f234
Martin Klevin <mogar85@hotmail.com>
2015-11-03 14:19:03 +0100

    added ArmySaves added Player (not finished) added enums minor changes to UnitProperties
    The buttons somewhat work :) you can end your turn.

--- Toy_box_wars_the_sand_box_conflict/Assets/Scripts/UnitProperties.cs
+++ Toy_box_wars_the_sand_box_conflict/Assets/Scripts/UnitProperties.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class UnitProperties : MonoBehaviour {
 
+    #region Fields
     [SerializeField]
     protected int health;
     [SerializeField]
@@ -11,6 +12,61 @@ public class UnitProperties : MonoBehaviour {
     protected int actionPoints;
     [SerializeField]
     protected float attackRange;
+    #endregion
+
+    #region Properties
+    public int Health
+    {
+        get
+        {
+            return health;
+        }
+
+        set
+        {
+            health = value;
+        }
+    }
+
+    public int Damage
+    {
+        get
+        {
+            return damage;
+        }
+
+        set
+        {
+            damage = value;
+        }
+    }
+
+    public int ActionPoints
+    {
+        get
+        {
+            return actionPoints;
+        }
+
+        set
+        {
+            actionPoints = value;
+        }
+    }
+
+    public float AttackRange
+    {
+        get
+        {
+            return attackRange;
+        }
+
+        set
+        {
+            attackRange = value;
+        }
+    }
+    #endregion
 
     public UnitProperties(int health, int damage, int actionPoints, float attackRange)
     {
